bee8d35f9f13965b9d6d926bc77253ce9fd4cf4c
gorv005 <gorv005@gmail.com>
2019-01-31 01:36:35 +0530

    region select

--- app/src/main/java/com/app/roadsafety/view/SelectRegionActivity.java
+++ app/src/main/java/com/app/roadsafety/view/SelectRegionActivity.java
@@ -1,7 +1,12 @@
 package com.app.roadsafety.view;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.app.roadsafety.R;
 
@@ -10,6 +15,18 @@ public class SelectRegionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
         setContentView(R.layout.activity_select_region);
+        changeStatusBarColor();
+    }
+
+    private void changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 }
