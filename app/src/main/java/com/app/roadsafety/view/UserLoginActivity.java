34b3cef8829877a6de3ad8ea7c000335fb95db6b
gorv005 <gorv005@gmail.com>
2019-01-31 01:28:51 +0530

    region select

--- app/src/main/java/com/app/roadsafety/view/UserLoginActivity.java
+++ app/src/main/java/com/app/roadsafety/view/UserLoginActivity.java
@@ -2,8 +2,13 @@ package com.app.roadsafety.view;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -25,10 +30,21 @@ public class UserLoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
         setContentView(R.layout.activity_user_login);
         ButterKnife.bind(this);
+        changeStatusBarColor();
     }
 
+    private void changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
     @OnClick(R.id.facebook_button)
     public void onViewClicked() {
         String applicationId =  getResources().getString(R.string.facebook_app_id);
