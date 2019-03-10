0e3eb4c7c41c8806e2374d7e947699a63a595c08
gorv005 <gorv005@gmail.com>
2019-03-10 17:06:16 +0530

    Location services

--- app/src/main/java/com/app/roadsafety/view/fragments/FragmentLocationServices.java
+++ app/src/main/java/com/app/roadsafety/view/fragments/FragmentLocationServices.java
@@ -6,25 +6,56 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.app.roadsafety.R;
+import com.app.roadsafety.view.MainActivity;
+import com.suke.widget.SwitchButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentLocationServices extends Fragment {
+public class FragmentLocationServices extends BaseFragment {
 
+
+    @BindView(R.id.switch_button)
+    SwitchButton switchButton;
+    @BindView(R.id.llNotification)
+    RelativeLayout llNotification;
+    Unbinder unbinder;
 
     public FragmentLocationServices() {
         // Required empty public constructor
     }
 
-
+    public static FragmentLocationServices newInstance(int instance) {
+        Bundle args = new Bundle();
+        args.putInt(ARGS_INSTANCE, instance);
+        FragmentLocationServices fragment = new FragmentLocationServices();
+        fragment.setArguments(args);
+        return fragment;
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location_services, container, false);
+        View view = inflater.inflate(R.layout.fragment_location_services, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((MainActivity) getActivity()).updateToolbarTitle(getString(R.string.location_services), true);
 
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
--- app/src/main/java/com/app/roadsafety/view/fragments/SettingsFragment.java
+++ app/src/main/java/com/app/roadsafety/view/fragments/SettingsFragment.java
@@ -1,18 +1,25 @@
 package com.app.roadsafety.view.fragments;
 
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.app.roadsafety.R;
 import com.app.roadsafety.utility.AppConstants;
 import com.app.roadsafety.utility.sharedprefrences.SharedPreference;
 import com.app.roadsafety.view.MainActivity;
+import com.app.roadsafety.view.UserLoginActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -77,8 +84,9 @@ public class SettingsFragment extends BaseFragment {
                 }
                 break;
             case R.id.rlLocationServices:
-               /* intent = new Intent(getActivity(), IncidentListReported.class);
-                startActivity(intent);*/
+                if (mFragmentNavigation != null) {
+                    mFragmentNavigation.pushFragment(FragmentLocationServices.newInstance(1));
+                }
                 break;
             case R.id.rlNotifications:
                  /*intent = new Intent(getActivity(), NotificationActivity.class);
@@ -95,7 +103,44 @@ public class SettingsFragment extends BaseFragment {
                 }
                 break;
             case R.id.rlLogout:
+                logoutDialog(getActivity(),getString(R.string.logout_confirm));
                 break;
         }
+    }
+
+
+    public void logoutDialog(Context context, String msg) {
+
+        final Dialog dialog = new Dialog(context, R.style.FullHeightDialog); //this is a reference to the style above
+        dialog.setContentView(R.layout.result_pop_up); //I saved the xml file above as yesnomessage.xml
+        dialog.setCancelable(true);
+        dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        TextView textView=(TextView)dialog.findViewById(R.id.tvMsg);
+        textView.setText(msg);
+        Button btnOk=(Button)dialog.findViewById(R.id.btnOk);
+        ImageView ivCross=(ImageView)dialog.findViewById(R.id.ivCross);
+        ivCross.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                gotoStart();
+            }
+        });
+//to set the message
+        dialog.show();
+    }
+
+    void gotoStart(){
+        Intent intent = new Intent(getActivity(), UserLoginActivity.class);
+        intent.putExtra(AppConstants.TAB_SELECTION,1);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
