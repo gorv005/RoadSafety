7b43fd6bd986af3a70da6647aa51b42f1675953f
gaurav.garg <gorv005@gmail.com>
2019-03-29 20:09:15 +0530

    Changes

--- app/src/main/java/com/app/roadsafety/view/ActivitySplash.java
+++ app/src/main/java/com/app/roadsafety/view/ActivitySplash.java
@@ -55,7 +55,7 @@ public class ActivitySplash extends AppCompatActivity {
         }
 
         setContentView(R.layout.activity_splash);
-          setLocale();
+        setLocale();
         changeStatusBarColor();
 
     }
