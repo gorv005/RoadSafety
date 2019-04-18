1c10dc1633410b6bb4732595d6079f3e81d3e8ca
gorv005 <gorv005@gmail.com>
2019-04-18 23:49:28 +0530

    release

--- app/src/main/java/com/app/roadsafety/view/fragments/FragmentInstruction.java
+++ app/src/main/java/com/app/roadsafety/view/fragments/FragmentInstruction.java
@@ -38,6 +38,7 @@ public class FragmentInstruction extends BaseFragment {
         fragment.setArguments(args);
         return fragment;
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
